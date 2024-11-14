8a62b1d3a928a5624ce979f150549bfe53e83452
Homan Qiu <thehuiq2006@gmail.com>
2024-11-13 21:07:03 -0800

    super short i'm tired ;-;

--- A Journey to Babylon/Assets/Scripts/Overworld/QuestManager.cs
+++ A Journey to Babylon/Assets/Scripts/Overworld/QuestManager.cs
@@ -6,21 +6,26 @@ using TMPro;
 
 public class QuestManager : MonoBehaviour
 {
-    [SerializeField] string MainQuest;
-    [SerializeField] List<string> SubQuest;
-
-    [Header("Quest System Req")]
     [SerializeField] TMP_Text MainQuestText;
-    [SerializeField] List<TMP_Text> SubQuestText;
-    
+    [SerializeField] List<TMP_Text> SubQuestObjects;
     void Start()
     {
-        
+        SetQuest("Become epic murderer", new List<string> {"murder 10 kids", "Murder 5 dogs", "murder 1 rock"});
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public void SetQuest(string MainQuest, List<string> SubQuest)
     {
+        MainQuestText.text = "MainQuest: " + MainQuest;
         
+        //reminder to fix up this code to allow for more subquest
+        for(int I = 0; I < SubQuestObjects.Count; I++)
+        {
+            SubQuestObjects[I].text = ""; 
+
+            if(I > SubQuestObjects.Count - 1) continue;
+            if(I > SubQuest.Count - 1) continue;
+            
+            SubQuestObjects[I].text = "-"+SubQuest[I];
+        }
     }
 }
