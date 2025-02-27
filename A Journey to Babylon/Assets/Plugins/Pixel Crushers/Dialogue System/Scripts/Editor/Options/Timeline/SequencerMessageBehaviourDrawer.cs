2a517f3292e3db7d1991e58a4124fb32014bcdec
3313556500 <3313556500qq@gmail.com>
2025-02-26 16:11:44 -0800

    new dialogue system 1.1

---
+++ A Journey to Babylon/Assets/Plugins/Pixel Crushers/Dialogue System/Scripts/Editor/Options/Timeline/SequencerMessageBehaviourDrawer.cs
@@ -0,0 +1,20 @@
+// Recompile at 2/14/2025 3:22:32 PM
+#if USE_TIMELINE
+#if UNITY_2017_1_OR_NEWER
+using UnityEditor;
+using UnityEngine;
+
+namespace PixelCrushers.DialogueSystem
+{
+
+    [CustomPropertyDrawer(typeof(SequencerMessageBehaviour))]
+    public class SequencerMessageBehaviourDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("message"));
+        }
+    }
+}
+#endif
+#endif
